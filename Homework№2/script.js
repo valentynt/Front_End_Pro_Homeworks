08578313c56d60d832fb52d954350fec093d6004
Valentyn Tyrtychko <valentyntyrtychko@gmail.com>
2020-04-05 23:29:58 +0300

    start use variables

---
+++ Homework№2/script.js
@@ -0,0 +1,5 @@
+let firstNameLastName = "Valentyn Tyrtychko";
+let myAge = 99;
+let messageUser = "Hello my friend";
+let proffer = messageUser + "," + firstNameLastName + "," + myAge;
+alert(proffer);
